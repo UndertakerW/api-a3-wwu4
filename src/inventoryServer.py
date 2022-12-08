fb331bd755984b7670c96cae6f6793112393a050
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2022-12-08 14:28:27 -0500

    Implement Part of the Service

--- src/inventoryServer.py
+++ src/inventoryServer.py
@@ -1,20 +1,37 @@
 from concurrent import futures
 import logging
 
+import os, sys
+# getting the name of the directory
+# where this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
 import grpc
 import service.book_pb2
 import service.inventoryItem_pb2
 import service.inventoryService_pb2
+import service.inventoryService_pb2_grpc
 
-class Greeter(service.inventoryService_pb2.):
+class InventoryService(service.inventoryService_pb2_grpc.InventoryServiceServicer):
 
-    def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+    def CreateBook(self, request, context):
+        return service.inventoryService_pb2.CreateBookResponse(
+            status=service.inventoryService_pb2.CreateBookStatus.SUCCESS,
+            message='Book with ISBN %s created successfully.' % request.book.isbn
+        )
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    service.inventoryService_pb2_grpc.add_InventoryServiceServicer_to_server(InventoryService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
