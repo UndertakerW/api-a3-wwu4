423508bf7a299f15542b048271f6135158a2a28c
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2022-12-14 01:22:45 -0500

    Add comments

--- src/inventoryServer.py
+++ src/inventoryServer.py
@@ -3,7 +3,6 @@ import logging
 
 import os, sys
 
-
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
@@ -16,13 +15,15 @@ import service.inventoryService_pb2_grpc
 # Please see db.py for more details
 import db
 
-port = 50051
 
 class InventoryService(service.inventoryService_pb2_grpc.InventoryServiceServicer):
 
+    # Create a book in the inventory
+    # request contains:
+    # Book book = 1
     def CreateBook(self, request, context):
 
-        # Check the mandatory field isbn
+        # Check the mandatory field book.isbn
 
         if request.book is None:
             return service.inventoryService_pb2.CreateBookResponse(
@@ -42,6 +43,7 @@ class InventoryService(service.inventoryService_pb2_grpc.InventoryServiceService
                 message='[Error] Book with ISBN %s exists.' % request.book.isbn
             )
 
+        # Create a new BookObject and add it to the database
         newBook = db.BookObject()
         newBook.isbn = request.book.isbn
         newBook.year = request.book.year
@@ -50,14 +52,18 @@ class InventoryService(service.inventoryService_pb2_grpc.InventoryServiceService
         newBook.title = request.book.title
         db.books[request.book.isbn] = newBook
 
-        # print(db.books[request.book.isbn])
-
+        # Return status and message
         return service.inventoryService_pb2.CreateBookResponse(
             status=service.inventoryService_pb2.ServiceStatus.SERVICE_SUCCESS,
             message='Book with ISBN %s created successfully.' % request.book.isbn
         )
 
+    # Create a book in the inventory
+    # request contains:
+    # string isbn = 1
     def GetBook(self, request, context):
+
+        # Check the mandatory field isbn
 
         if request.isbn is None or len(request.isbn) == 0:
             return service.inventoryService_pb2.GetBookResponse(
@@ -71,29 +77,42 @@ class InventoryService(service.inventoryService_pb2_grpc.InventoryServiceService
                 message='[Error] Book with ISBN %s does not exist.' % request.isbn
             )
 
+        # Get the BookObject from database
+        bookObject = db.books[request.isbn]
+
+        # Craft a Book for proto
         book = service.inventoryService_pb2.book__pb2.Book(
             isbn=request.isbn,
-            title=db.books[request.isbn].title,
-            author=db.books[request.isbn].author,
-            year=db.books[request.isbn].year,
-            genre=db.books[request.isbn].genre
+            title=bookObject.title,
+            author=bookObject.author,
+            year=bookObject.year,
+            genre=bookObject.genre
         )
 
+        # Return status, message, and the Book
         return service.inventoryService_pb2.GetBookResponse(
             status=service.inventoryService_pb2.ServiceStatus.SERVICE_SUCCESS,
             message='Book with ISBN %s returned successfully.' % request.isbn,
             book=book
         )
 
-
-def serve():
+# Start the server
+def startServer(port):
+    # Instantiate the server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # Add the service
     service.inventoryService_pb2_grpc.add_InventoryServiceServicer_to_server(InventoryService(), server)
-    server.add_insecure_port('[::]:'+str(port))
+    # Set up the port
+    server.add_insecure_port('[::]:' + str(port))
+    # Start the server
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
     logging.basicConfig()
-    serve()
+    if len(sys.argv) == 2:
+        startServer(sys.argv[2])
+    else:
+        # Default port = 50051
+        startServer(50051)
