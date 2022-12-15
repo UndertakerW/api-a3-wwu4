9416e82b4be01604d77d6ec6bd147ce10bcb971e
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2022-12-14 23:15:27 -0500

    Update test.py

--- client/test.py
+++ client/test.py
@@ -15,10 +15,10 @@ import service.inventoryService_pb2
 TEST_SERVER = True
 
 def set_up_mock():
-    # Create Mock
+    # Create mock API client
     client = unittest.mock.Mock()
 
-    # Prepare mock behavior
+    # Define mock behavior
 
     book1 = service.inventoryService_pb2.book__pb2.Book(
         isbn='978–0–07–340320–5',
@@ -60,7 +60,7 @@ class TestGetBookTitles(unittest.TestCase):
         # Init the super class
         super(TestGetBookTitles, self).__init__(*args, **kwargs)
 
-    # Test get_book_titles.getTitles() with a Mock of API class interface
+    # Test get_book_titles.getTitles() with a mock client
     def test_get_titles_from_mock(self):
         # Create Mock
         client = set_up_mock()
@@ -75,7 +75,7 @@ class TestGetBookTitles(unittest.TestCase):
         self.assertEqual(expected, actual)
 
 
-    # Test get_book_titles.getTitles() with a Mock of API class interface
+    # Test get_book_titles.getTitles() with a mock client
     # Mock ISBN not found
     def test_get_titles_from_mock_isbn_not_found(self):
         # Create Mock
@@ -91,7 +91,7 @@ class TestGetBookTitles(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    # Test get_book_titles.getTitles() with a real server
+    # Test get_book_titles.getTitles() with a live server
     @unittest.skipUnless(TEST_SERVER, 'Test with server')
     def test_get_titles_from_server(self):
         # Instantiate client
@@ -106,7 +106,7 @@ class TestGetBookTitles(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    # Test get_book_titles.getTitles() with a real server
+    # Test get_book_titles.getTitles() with a live server
     # Test ISBN not found
     @unittest.skipUnless(TEST_SERVER, 'Test with server')
     def test_get_titles_from_server_isbn_not_found(self):
